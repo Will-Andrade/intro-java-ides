fd7fe0149f9ddf5985f0b59ffce75f750b6e9ddc
Will-Andrade <willian.santos.andra@outlook.com>
2023-02-28 13:19:08 -0400

    feat: creates SecondProgram & Cat classes

---
+++ src/secondProgram/SecondProgram.java
@@ -0,0 +1,11 @@
+package secondProgram;
+
+import secondProgram.model.Cat;
+
+public class SecondProgram {
+	public static void main(String args[]) {
+		Cat cat = new Cat("Snuffles", "Brown", 5);
+		
+		System.out.print(cat);
+	}
+}
