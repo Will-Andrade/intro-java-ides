9d920457af1725ee0c1d0a9f7a9cfbf182a14e96
Will-Andrade <willian.santos.andra@outlook.com>
2023-02-28 14:24:13 -0400

    feat: first Intellij project created

---
+++ src/willPackage/IntellijProgram.java
@@ -0,0 +1,13 @@
+package willPackage;
+
+import willPackage.model.Cat;
+
+public class IntellijProgram {
+  public static void main(String[] args) {
+    /*int firstNumber = 22;
+    int secondNumber = 32;
+    System.out.println("Hello Intellij! " + (firstNumber + secondNumber));*/
+    Cat kitty = new Cat();
+    System.out.println(kitty);
+  }
+}
